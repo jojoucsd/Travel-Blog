7fb159b1e3b1034c8d690aa05e6e406e672f2a14
Ling Lu <jojoucsd@gmail.com>
2020-02-12 15:23:39 -0800

    updated config-gatsby.js and gitignore in blog

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -16,7 +16,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: process.env.DEPLOY_URL
+        ? 'https://family-travel-blog.herokuapp.com'
+        : 'http://localhost:1337',
         contentTypes: [
           `article`,
           `user`,
