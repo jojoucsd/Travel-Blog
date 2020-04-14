4594f036286fb57f58e02ec43db820ec15f84288
Ling Lu <jojoucsd@gmail.com>
2020-04-13 21:41:31 -0700

    gatsby clean and cluster test

--- blog/src/pages/clusters.js
+++ blog/src/pages/clusters.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import Map from '../components/Map/cluster'
 import Layout from '../components/layout' 
-import ClusterMap from '../components/Map'
 
 const ClusterPage = () => (
   <Layout content="map">
-    <ClusterMap />
+    <Map />
   </Layout>
   )
   
